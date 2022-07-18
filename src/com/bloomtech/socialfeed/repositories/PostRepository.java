e261db6b94837f5f79b6150051a26dab072ea062
jcassidy91 <jamie@Lambda-School-Jamies-MacBook-Pro.local>
2022-07-18 13:23:59 -0700

    initial

---
+++ src/com/bloomtech/socialfeed/repositories/PostRepository.java
@@ -0,0 +1,39 @@
+package com.bloomtech.socialfeed.repositories;
+
+import com.bloomtech.socialfeed.models.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PostRepository {
+    private static final String POST_DATA_PATH = "src/resources/PostData.json";
+
+    public PostRepository() {
+    }
+
+    public List<Post> getAllPosts() {
+        //TODO: return all posts from the PostData.json file
+        return new ArrayList<>();
+    }
+
+    public List<Post> findByUsername(String username) {
+        return getAllPosts()
+                .stream()
+                .filter(p -> p.getUsername().equals(username))
+                .collect(Collectors.toList());
+    }
+
+    public List<Post> addPost(Post post) {
+        //This logic is required during Mastery Task 1 and can be changed if needed after.
+        List<Post> allPosts = new ArrayList<>();
+        allPosts.add(post);
+
+        //TODO: Create and Add a new Post
+
+        //TODO: Write the new Post data to the PostData.json file
+
+        //TODO: Return an updated list of all posts
+        return allPosts;
+    }
+}
